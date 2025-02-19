22bd6ea5564ec4ae8c8dff6f67752cb633d1eaff
Jonathan Leohr <40473237+jleohr@users.noreply.github.com>
2025-02-19 08:06:18 -0600

    Add files via upload

---
+++ Lab24d.java
@@ -0,0 +1,28 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Lab24d
+{
+	public static void main( String args[] ) throws IOException
+	{
+		Scanner file = new Scanner (new File("Lab24d.dat"));
+
+
+
+
+
+
+
+
+	}
+}
+
+
+
